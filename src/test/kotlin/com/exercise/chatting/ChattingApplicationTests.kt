5a1148bfcc3544beb418a5e490e0d9cbd86023f8
sangwoo420 <sangwoo420@gmail.com>
2022-05-26 13:12:55 +0900

    init

---
+++ src/test/kotlin/com/exercise/chatting/ChattingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exercise.chatting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChattingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
